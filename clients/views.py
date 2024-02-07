430e3ac23ef7469677398c9ec300d38c480445b9
Angel David Castro <davangcas@gmail.com>
2024-02-07 20:15:00 -0300

    fix: clients crud improvements

--- clients/views.py
+++ clients/views.py
@@ -6,9 +6,16 @@ from clients.forms import ClientCreateForm, ClientEditForm
 from clients.models import Client
 
 
-class ClientListView(ListView, LoginRequiredMixin):
+class ClientListView(ListView):
     model = Client
     template_name = "clients/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Clientes"
+        context["create_url"] = reverse_lazy("clients:create")
+        context["active_section"] = "clients"
+        return context
 
 
 class ClientCreateView(CreateView):
@@ -17,6 +24,13 @@ class ClientCreateView(CreateView):
     success_url = reverse_lazy("clients:list")
     form_class = ClientCreateForm
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Crear cliente"
+        context["cancel_url"] = reverse_lazy("clients:list")
+        context["active_section"] = "clients"
+        return context
+
 
 class ClientUpdateView(UpdateView):
     model = Client
@@ -24,8 +38,22 @@ class ClientUpdateView(UpdateView):
     success_url = reverse_lazy("clients:list")
     form_class = ClientEditForm
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Actualizar cliente"
+        context["cancel_url"] = reverse_lazy("clients:list")
+        context["active_section"] = "clients"
+        return context
+
 
 class ClientDeleteView(DeleteView):
     model = Client
     template_name = "clients/delete.html"
     success_url = reverse_lazy("clients:list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Eliminar cliente"
+        context["cancel_url"] = reverse_lazy("clients:list")
+        context["active_section"] = "clients"
+        return context
