2d05b70c5ac312d13b628a3e2f769a01bdb4f08e
Angel David Castro <davangcas@gmail.com>
2024-02-09 01:29:37 -0300

    feat: login and logout for users

--- config/settings/base.py
+++ config/settings/base.py
@@ -98,11 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = "es-ar"
-
 TIME_ZONE = "America/Argentina/La_Rioja"
-
 USE_I18N = True
-
 USE_TZ = True
 
 # Default primary key field type
@@ -114,6 +111,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# login and logout
+LOGIN_REDIRECT_URL = "dashboard:index"
+LOGIN_URL = "dashboard:login"
 AUTH_USER_MODEL = "users.User"
+
+# crispy forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
