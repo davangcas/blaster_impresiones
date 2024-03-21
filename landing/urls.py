dd1b0c8458b14dc0672d481c05412bd8a2831dbe
Angel David Castro <davangcas@gmail.com>
2024-03-21 10:26:24 -0300

    feat: terms and conditions

--- landing/urls.py
+++ landing/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 
-from landing.views import IndexView, AboutView
+from landing.views import (
+    AboutView,
+    IndexView,
+    ModelingServiceView,
+    PrintServiceView,
+    TermsAndConditionsView,
+)
 
 app_name = "landing"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("about/", AboutView.as_view(), name="about"),
+    path("print-service/", PrintServiceView.as_view(), name="print_service"),
+    path("modeling-service/", ModelingServiceView.as_view(), name="modeling_service"),
+    path(
+        "terms-and-conditions/",
+        TermsAndConditionsView.as_view(),
+        name="terms_and_conditions",
+    ),
 ]
--- landing/views.py
+++ landing/views.py
@@ -24,3 +24,33 @@ class AboutView(TemplateView):
         context["title"] = "Acerca de nosotros"
         context["active_section"] = "about"
         return context
+
+
+class PrintServiceView(TemplateView):
+    template_name = "landing/prints.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Servicio de impresión 3D"
+        context["active_section"] = "services"
+        return context
+
+
+class ModelingServiceView(TemplateView):
+    template_name = "landing/modeling.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Modelado de piezas 3D"
+        context["active_section"] = "services"
+        return context
+
+
+class TermsAndConditionsView(TemplateView):
+    template_name = "landing/terms.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Términos y condiciones"
+        context["active_section"] = "terms"
+        return context
